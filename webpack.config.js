ba18da7776ddad15e83e1dac4b13e6e0e2a87d1f
weboffools <eliot@weboffools.com>
2023-11-10 19:49:06 -0700

    Add needed config to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,41 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-          filename: '[name].bundle.js',
-          path: path.resolve(__dirname, 'dist'),
-        },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'City of Pigs',
-      }),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'City of Pigs',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
