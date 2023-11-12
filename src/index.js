49194cb79339267bc8c2d9c81c59003aeb8610bd
weboffools <eliot@weboffools.com>
2023-11-12 11:05:51 -0700

    Add event handlers for tabs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import './style.css';
 import mainComponent from './home.js';
+import makeHeader from './header.js';
+import menuDiv from './menu.js';
 
+document.body.appendChild(makeHeader());
 document.body.appendChild(mainComponent());
+
+const changeContent = (newTab) => {
+  const mainContent = document.querySelector('.main-content');
+  mainContent.replaceChildren();
+  mainContent.appendChild(newTab);
+};
+
+const homeTab = document.querySelector('.home-tab');
+const menuTab = document.querySelector('.menu-tab');
+// const contactTab = document.querySelector('.contact-tab');
+
+homeTab.addEventListener('click', () => {
+  changeContent(mainComponent());
+});
+
+menuTab.addEventListener('click', () => {
+  changeContent(menuDiv());
+});
+
+//contactTab.addEventListener('click', (e) => {
+//  changeContent();
+//});
+
